6d90d70164e1d532d63eb6b3201606db5f8300ef
Balaramansethu <baalaaraman@gmail.com>
2025-04-17 14:47:15 +0530

    intial commit to save folder structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <div>This commit is to save folder structure</div>
+        </React.Fragment>
+    )
+}
+
+export default App
